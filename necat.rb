48c779f3e8408246e4a9de4795004bc4639e77bf
Dan Fitch <dgfitch@gmail.com>
2010-02-21 02:03:54 -0600

    Some clicking is working but flipping is very wrong still

--- necat.rb
+++ necat.rb
@@ -2,10 +2,20 @@ SIZE = 1.0
 WIDTH = 800 * SIZE
 HEIGHT = 600 * SIZE
 
+BOARD_WIDTH = 5
+BOARD_HEIGHT = 4
+#BOARD_WIDTH = 10
+#BOARD_HEIGHT = 8
+
+SPEED = 0.01
+
 class Sketch < Processing::App
   
+
   class Board
-    def initialize(w = 10, h = 8)
+    attr_accessor :mouse_pressed
+
+    def initialize(w = BOARD_WIDTH, h = BOARD_HEIGHT)
       @w = w
       @h = h
 
@@ -23,8 +33,33 @@ class Sketch < Processing::App
       end
     end
 
-    def neighbor(row, col, n)
+    def opposite_index n
+      case n
+      when 0: 3
+      when 1: 4
+      when 2: 5
+      when 3: 0
+      when 4: 1
+      when 5: 2
+      end
+    end
+
+    def neighbor_hex(target, n)
       # n is edge number, where 0 is the upper right edge and going clockwise
+      row = nil
+      col = nil
+
+      @state.each_with_index do |r, i|
+        r.each_with_index do |hex, j|
+          if hex == target then
+            row = i
+            col = j
+          end
+        end
+      end
+
+      return nil unless row
+
       case n
       when 0
         nrow = row - 1
@@ -44,61 +79,93 @@ class Sketch < Processing::App
       when 5
         nrow = row - 1
         ncol = col - 1
+      else
+        return nil
       end
 
       return nil if nrow > @h or nrow < 0 or ncol > @w or ncol < 0
-      return state[nrow][ncol]
+      return nil unless @state[nrow]
+      return @state[nrow][ncol]
     end
 
-    def advance
+    def neighbor_edge(target, n)
+      hex = neighbor_hex(target, n)
+      return nil unless hex
+      hex.edges[opposite_index(n)]
+    end
+
+    def for_all
       @state.each do |row|
         row.each do |hex|
-          hex.advance_flip
-        end
-      end
-      @state.each do |row|
-        row.each do |hex|
-          hex.advance_rotate
+          yield hex if hex
         end
       end
     end
 
-    def draw
+    def reset_activity
+      for_all do |hex|
+        hex.reset_activity
+      end
+    end
+
+    def advance_all
+      reset_activity
+      for_all do |hex|
+        hex.advance_rotate
+      end
+      for_all do |hex|
+        hex.advance_flip
+      end
+    end
+
+    def draw fps
+      r = 350 / @w
       @state.each_with_index do |row, i|
         row.each_with_index do |hex, j|
-          hex.draw(30, 100 + j * 28, 100 + i * 48) if hex
+          hex.draw(fps, r, 10 + r + j * ( 350 * SIZE / @w ), 20 + r + i * ( 500 * SIZE / @h )) if hex
         end
       end
     end
   end
 
   class Edge
-    attr_reader :hex
+    attr_accessor :hex
     def initialize(hex)
       @hex = hex
+      @active = true
+      @time = 0
     end
 
     def prepare
-      stroke_width 2
-      @focus = false
+      if @active then
+        stroke_width 4
+      else
+        stroke_width 1
+      end
+      
       if @hex.focus then
         # TODO: Figure out of this edge is in focus
-        index = @hex.edges.index(self)
-        @alpha = 100
+        @focus = true
+        #index = @hex.edges.index(self)
+        @alpha = 100 - @time
       else
-        @alpha = 200
+        @focus = false
+        @alpha = 200 - @time
       end
     end
 
-    def draw(s,h,r)
-      stroke_width 1
-      stroke 40
-      line 0, 0, -4, -4
+    def draw(fps,s,h,r)
+      offset = (-40 * SIZE / BOARD_WIDTH)
+
+      #stroke_width 1
+      #stroke 40
 
       prepare
 
+      line 0, 0, offset, offset
+
       begin_shape
-      vertex -4, -4
+      vertex offset, offset
       case @hex.edges.index(self)
       when 0
         vertex 0, -r
@@ -119,8 +186,31 @@ class Sketch < Processing::App
         vertex -s, h - r
         vertex 0, -r
       end
-      vertex -4, -4
+      vertex offset, offset
       end_shape
+
+      if @focus and @hex.board.mouse_pressed == LEFT then
+        self.advance
+      elsif not @active then
+        @time = @time - (fps * SPEED)
+        if @time < 0
+          activate
+        end
+      end
+    end
+
+    def activate
+      @hex.advance
+      @active = true 
+    end
+
+    def reset_activity
+      @active = true
+    end
+
+    def inactivate
+      @active = false
+      @time = 100
     end
   end
 
@@ -130,6 +220,17 @@ class Sketch < Processing::App
       stroke 20, 50, 200, @alpha
       fill 40, 70, 220, @alpha
     end
+
+    def activate
+      @active = true 
+    end
+
+    def advance
+      return unless @active
+      inactivate
+      puts "clockwise"
+      @hex.rotate
+    end
   end
 
   class FlipEdge < Edge
@@ -137,6 +238,23 @@ class Sketch < Processing::App
       super
       stroke 200, 50, 50, @alpha
       fill 220, 70, 70, @alpha
+    end
+
+    def advance
+      return unless @active
+      puts "flip"
+      index = @hex.index_of self
+      opposite = @hex.board.opposite_index index
+      other_hex = @hex.neighbor_of self
+      return unless other_hex
+      other_edge = other_hex.edges[opposite]
+      if not other_edge or not other_edge.kind_of? FlipEdge then
+        inactivate
+        other_hex.edges[opposite] = self
+        @hex.edges[index] = other_edge
+        other_edge.hex = @hex if other_edge
+        @hex = other_hex
+      end
     end
   end
 
@@ -158,7 +276,7 @@ class Sketch < Processing::App
       end
     end
     
-    def draw(r, x, y)
+    def draw(fps, r, x, y)
       @r ||= r
       @h ||= @r / 2
       @s ||= Math.sin(radians(60)) * @r
@@ -193,20 +311,58 @@ class Sketch < Processing::App
       line @s, @h - @r, -@s, @r - @h
       line @s, @r - @h, -@s, @h - @r
       
-      @edges.each { |edge| edge.draw(@s,@h,@r) if edge }
+      @edges.each { |edge| edge.draw(fps,@s,@h,@r) if edge }
       
       pop_matrix
     end
 
     def neighbor(n)
-      @board.neighbor(@x, @y, n)
+      @board.neighbor_hex(self, n)
     end
 
+    def neighbor_edge(n)
+      @board.neighbor_edge(self, n)
+    end
+
+    def neighbor_of(edge)
+      index = @edges.index(edge)
+      neighbor(index)
+    end
+
+    def index_of(edge)
+      @edges.index(edge)
+    end
+
+    def reset_activity
+      @edges.each do |e|
+        e.reset_activity if e
+      end
+    end
+
+    def advance
+      advance_flip
+      advance_rotate
+    end
 
     def advance_flip
+      @edges.each do |e|
+        if e and e.kind_of? FlipEdge then
+          e.advance
+        end
+      end
     end
 
     def advance_rotate
+      @edges.each do |e|
+        if e and e.kind_of? ClockwiseEdge then
+          e.advance
+        end
+      end
+    end
+
+    def rotate
+      x = @edges.pop
+      @edges.unshift x
     end
   end
   
@@ -220,34 +376,36 @@ class Sketch < Processing::App
 
     @board = Board.new
 
-    $frame_time = nil
+    @frame_time = nil
     $frame_count = 0
   end
 
 
   def draw
     t = Time.now
-    fps = 1.0 / (t - $frame_time) if $frame_time
-    $frame_time = t
+    fps = 1.0 / (t - @frame_time) if @frame_time
+    @frame_time = t
     $frame_count += 1
 
     background 50
 
     fill 240
-    @board.draw
+
+    @board.draw fps
   end
 
   def key_pressed
     case key
-      when 'a' then @board.advance
+      when 'a' then @board.advance_all
     end
   end
 
   def mouse_pressed
-    case mouse_button
-    when LEFT then puts "L"
-    when RIGHT then puts "R"
-    end
+    @board.mouse_pressed = mouse_button
+  end
+
+  def mouse_released
+    @board.mouse_pressed = nil
   end
 end
 

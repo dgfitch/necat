f348840808700bdab5ae1613ff269eebaa55961d
Dan Fitch <dan@rapt.us>
2010-03-28 17:52:00 -0500

    Hacking on correctness, changing to hash instead of array, adding another edge type

--- necat.rb
+++ necat.rb
@@ -2,12 +2,13 @@ SIZE = 1.0
 WIDTH = 800 * SIZE
 HEIGHT = 600 * SIZE
 
-BOARD_WIDTH = 5
-BOARD_HEIGHT = 4
-#BOARD_WIDTH = 10
-#BOARD_HEIGHT = 8
+#BOARD_WIDTH = 15
+#BOARD_HEIGHT = 12
+BOARD_WIDTH = 6
+BOARD_HEIGHT = 5
 
-SPEED = 0.01
+SPEED = 0.1
+DENSITY = 0.4
 
 class Sketch < Processing::App
   
@@ -111,10 +112,7 @@ class Sketch < Processing::App
     def advance_all
       reset_activity
       for_all do |hex|
-        hex.advance_rotate
-      end
-      for_all do |hex|
-        hex.advance_flip
+        hex.advance
       end
     end
 
@@ -126,14 +124,135 @@ class Sketch < Processing::App
         end
       end
     end
+
+    def update fps
+      for_all do |hex|
+        hex.update fps
+      end
+    end
+  end
+
+  class Hex
+    attr_accessor :x, :y, :r
+    attr_reader :edges, :board, :focus
+    def initialize(board, x = 0, y = 0)
+      @board = board
+      @edges = { 0 => random_edge,
+                 1 => random_edge,
+                 2 => random_edge,
+                 3 => random_edge,
+                 4 => random_edge,
+                 5 => random_edge }
+    end
+
+    def random_edge
+      if random(1) > (1.0 - DENSITY) then
+        if random(1) < 0.3 then 
+          ClockwiseEdge.new(self)
+        elsif random(1) < 0.3 then
+          CounterClockwiseEdge.new(self)
+        else
+          FlipEdge.new(self)
+        end
+      else
+        nil
+      end
+    end
+    
+    def draw(fps, r, x, y)
+      @r ||= r
+      @h ||= @r / 2
+      @s ||= Math.sin(radians(60)) * @r
+
+      d = dist(x, y, mouseX, mouseY)
+      # TODO: Should do an actual intersection if we're close
+      if d < @r then
+        @focus = true
+        fill 240
+      else
+        @focus = false
+        fill 200
+      end
+
+      push_matrix
+      translate x, y
+      stroke_width 2
+      stroke 0
+      begin_shape
+      vertex 0, -@r
+      vertex @s, @h - @r
+      vertex @s, @r - @h
+      vertex 0, @r
+      vertex -@s, @r - @h
+      vertex -@s, @h - @r
+      vertex 0, -@r
+      end_shape
+
+      stroke_width 1
+      stroke 100
+      line 0, -@r, 0, @r
+      line @s, @h - @r, -@s, @r - @h
+      line @s, @r - @h, -@s, @h - @r
+      
+      @edges.values.compact.each { |edge| edge.draw(fps,@s,@h,@r) }
+      
+      pop_matrix
+    end
+
+    def update(fps)
+      @edges.values.compact.each {|x| x.update fps }
+    end
+
+    def neighbor(n)
+      @board.neighbor_hex(self, n)
+    end
+
+    def neighbor_edge(n)
+      @board.neighbor_edge(self, n)
+    end
+
+    def neighbor_of(edge)
+      index = @edges.index(edge)
+      neighbor(index)
+    end
+
+    def index_of(edge)
+      @edges.values.index(edge)
+    end
+
+    def reset_activity
+      @edges.values.each do |e|
+        e.reset_activity if e
+      end
+    end
+
+    def advance
+      puts "Hex rotating"
+      @edges.values.compact.sort_by{|e| e.priority}.each do |edge|
+        edge.advance
+      end
+    end
+
+    def rotate_clockwise
+      @edges[1], @edges[2], @edges[3], @edges[4], @edges[5], @edges[0], =
+      @edges[0], @edges[1], @edges[2], @edges[3], @edges[4], @edges[5]
+    end
+
+    def rotate_counterclockwise
+      @edges[0], @edges[1], @edges[2], @edges[3], @edges[4], @edges[5] =
+      @edges[1], @edges[2], @edges[3], @edges[4], @edges[5], @edges[0]
+    end
   end
 
   class Edge
     attr_accessor :hex
+    attr_accessor :priority
+
     def initialize(hex)
       @hex = hex
       @active = true
       @time = 0
+      @priority = 1
     end
 
     def prepare
@@ -188,7 +307,9 @@ class Sketch < Processing::App
       end
       vertex offset, offset
       end_shape
+    end
 
+    def update(fps)
       if @focus and @hex.board.mouse_pressed == LEFT then
         self.advance
       elsif not @active then
@@ -214,11 +335,40 @@ class Sketch < Processing::App
     end
   end
 
-  class ClockwiseEdge < Edge
+  class CounterClockwiseEdge < Edge
+    def initialize(hex)
+      super
+      @priority = 2
+    end
+
     def prepare
       super
-      stroke 20, 50, 200, @alpha
-      fill 40, 70, 220, @alpha
+      stroke 70, 20, 200, @alpha
+      fill 120, 30, 220, @alpha
+    end
+
+    def activate
+      @active = true 
+    end
+
+    def advance
+      return unless @active
+      inactivate
+      puts "counterclockwise"
+      @hex.rotate_counterclockwise
+    end
+  end
+
+  class ClockwiseEdge < Edge
+    def initialize(hex)
+      super
+      @priority = 2
+    end
+
+    def prepare
+      super
+      stroke 20, 70, 200, @alpha
+      fill 40, 120, 220, @alpha
     end
 
     def activate
@@ -229,11 +379,16 @@ class Sketch < Processing::App
       return unless @active
       inactivate
       puts "clockwise"
-      @hex.rotate
+      @hex.rotate_clockwise
     end
   end
 
   class FlipEdge < Edge
+    def initialize(hex)
+      super
+      @priority = 1
+    end
+
     def prepare
       super
       stroke 200, 50, 50, @alpha
@@ -242,129 +397,25 @@ class Sketch < Processing::App
 
     def advance
       return unless @active
-      puts "flip"
       index = @hex.index_of self
       opposite = @hex.board.opposite_index index
       other_hex = @hex.neighbor_of self
-      return unless other_hex
-      other_edge = other_hex.edges[opposite]
-      if not other_edge or not other_edge.kind_of? FlipEdge then
-        inactivate
-        other_hex.edges[opposite] = self
-        @hex.edges[index] = other_edge
-        other_edge.hex = @hex if other_edge
-        @hex = other_hex
+      if other_hex then
+        puts "flip found other hex"
+        other_edge = other_hex.edges[opposite]
+        if not other_edge or not other_edge.kind_of? FlipEdge then
+          inactivate
+          other_hex.edges[opposite] = self
+          @hex.edges[index] = other_edge
+          other_edge.hex = @hex if other_edge
+          @hex = other_hex
+        end
+      else
+        puts "flip did not find hex"
       end
     end
   end
 
-  class Hex
-    attr_accessor :x, :y, :r
-    attr_reader :edges, :board, :focus
-    def initialize(board, x = 0, y = 0)
-      @board = board
-      @edges = [ random_edge, random_edge, random_edge, random_edge, random_edge, random_edge ]
-    end
-
-    def random_edge
-      if random(1) > 0.9 then
-        ClockwiseEdge.new(self)
-      elsif random(1) > 0.9 then
-        FlipEdge.new(self)
-      else
-        nil
-      end
-    end
-    
-    def draw(fps, r, x, y)
-      @r ||= r
-      @h ||= @r / 2
-      @s ||= Math.sin(radians(60)) * @r
-
-      d = dist(x, y, mouseX, mouseY)
-      # TODO: Should do an actual intersection if we're close
-      if d < @r then
-        @focus = true
-        fill 240
-      else
-        @focus = false
-        fill 200
-      end
-
-      push_matrix
-      translate x, y
-      stroke_width 2
-      stroke 0
-      begin_shape
-      vertex 0, -@r
-      vertex @s, @h - @r
-      vertex @s, @r - @h
-      vertex 0, @r
-      vertex -@s, @r - @h
-      vertex -@s, @h - @r
-      vertex 0, -@r
-      end_shape
-
-      stroke_width 1
-      stroke 100
-      line 0, -@r, 0, @r
-      line @s, @h - @r, -@s, @r - @h
-      line @s, @r - @h, -@s, @h - @r
-      
-      @edges.each { |edge| edge.draw(fps,@s,@h,@r) if edge }
-      
-      pop_matrix
-    end
-
-    def neighbor(n)
-      @board.neighbor_hex(self, n)
-    end
-
-    def neighbor_edge(n)
-      @board.neighbor_edge(self, n)
-    end
-
-    def neighbor_of(edge)
-      index = @edges.index(edge)
-      neighbor(index)
-    end
-
-    def index_of(edge)
-      @edges.index(edge)
-    end
-
-    def reset_activity
-      @edges.each do |e|
-        e.reset_activity if e
-      end
-    end
-
-    def advance
-      advance_flip
-      advance_rotate
-    end
-
-    def advance_flip
-      @edges.each do |e|
-        if e and e.kind_of? FlipEdge then
-          e.advance
-        end
-      end
-    end
-
-    def advance_rotate
-      @edges.each do |e|
-        if e and e.kind_of? ClockwiseEdge then
-          e.advance
-        end
-      end
-    end
-
-    def rotate
-      x = @edges.pop
-      @edges.unshift x
-    end
-  end
   
 
   def setup
@@ -391,6 +442,7 @@ class Sketch < Processing::App
 
     fill 240
 
+    @board.update fps
     @board.draw fps
   end
 

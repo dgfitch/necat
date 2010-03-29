8d667ff73771c89b5ecb1eda66621bf8d27d5385
Dan Fitch <dgfitch@gmail.com>
2010-03-28 21:10:30 -0500

    board spec

--- board.rb
+++ board.rb
@@ -1,7 +1,17 @@
+require 'hex'
 
 class Board
-  attr_accessor :hexes
+  attr_reader :hexes, :center
   def initialize n
-    @hexes = n
+    @hexes = []
+    @center = Hex.new self
+    (1..n).each do |i|
+      current_hexes = @hexes.clone
+      current_hexes.each {|x| x.expand}
+    end
   end
+  def add x
+    @hexes << x
+  end
+
 end
--- spec/spec_board.rb
+++ spec/spec_board.rb
@@ -3,12 +3,27 @@ require 'board.rb'
 describe Board do
   it "has 1 hex for radius 0" do
     board = Board.new 0
-    board.hexes.should == 1
+    board.hexes.length.should == 1
+  end
+
+  it "has 7 hexes for radius 1" do
+    board = Board.new 1
+    board.hexes.length.should == 7
   end
 
   it "has 61 hexes for radius 4" do
     board = Board.new 4
-    board.hexes.should == 61
+    board.hexes.length.should == 61
   end
+
+  it "has the correct aliases" do
+    board = Board.new 2
+    c = board.center
+    c.w.e.should == c
+    c.sw.ne.should == c
+    c.w.w.e.e.should == c
+    c.sw.w.should == c.w.sw
+  end
+
 end
 
